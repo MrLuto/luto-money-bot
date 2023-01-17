4873de041d1379e741e8360b43f8d460c68b7407
MrLuto <mmudde2@gmail.com>
2023-01-17 13:00:00 +0100

    update manager completed

--- sites/Update Manager.js
+++ sites/Update Manager.js
@@ -1,5 +1,6 @@
 var terminal = require("../terminal.js");
 const delay = require('delay');
+const shell = require('shelljs');
 
 var run = async function run(){
     terminal.log("clear", "Update Manager");
@@ -7,7 +8,8 @@ var run = async function run(){
     await delay(500);
     terminal.log("clear", "Update Manager");
     terminal.log("add", "Update Manager", 'Running updates');
-    await delay(500);
+    shell.exec('git pull');
+    await delay(10000);
     terminal.log("clear", "Update Manager");
     terminal.log("add", "Update Manager", 'no more updates to run');
     await delay(500);
